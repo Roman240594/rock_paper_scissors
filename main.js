8751e6ebc0ac02fe8ef2c25c01542dfc8080edc8
Roman <linevychroman@gmail.com>
2022-09-04 13:59:01 +0300

    create function add userImage

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
-let user = document.querySelectorAll('.name');
+const user = document.querySelectorAll('.name');
+const btn = document.querySelector('.sign__buttons');
+const userImage = document.querySelector('.choose__img-user');
 
 let userName;
+
+const signArray = [{
+        'name': 'Rock',
+        'image': 'image/rock.png'
+    },
+    {
+        'name': 'Paper',
+        'image': 'image/paper.png'
+    },
+    {
+        'name': 'Scissors',
+        'image': 'image/csissors.png'
+    }
+];
+
 
 let write = setTimeout(() => {
     userName = prompt('What is your name?');
@@ -18,3 +35,20 @@ let writeUserName = () => {
         }
     }
 };
+
+let choosedSign = () => {
+    let key = event.target.textContent;
+    for (let i = 0; i < signArray.length; i++) {
+        if (key === signArray[i].name) {
+            addImage(signArray[i].image)
+            console.log(key);
+        }
+    }
+}
+
+function addImage(link) {
+    userImage.src = link;
+}
+
+
+btn.addEventListener('click', choosedSign)
